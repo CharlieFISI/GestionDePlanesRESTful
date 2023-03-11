e5200d6ae9b05ffb9f7a917cd3b9ba2fceaf33e9
LinuxChar <charlie.elizarbe@unmsm.edu.pe>
2023-03-11 13:30:46 -0500

    API RESTful Gestion de Planes

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,8 @@
 import express, { Application } from 'express'
-import cors from 'cors'
 import indexRouter from './index'
 import ingresoRouter from './routes/ingresos'
 import planRouter from './routes/planes'
 import planingresoRouter from './routes/planesingresos'
-
-const allowedOrigins = ['http://localhost:3000']
-const options: cors.CorsOptions = {
-  origin: allowedOrigins
-}
 
 export class App {
   private readonly app: Application
@@ -26,7 +20,6 @@ export class App {
 
   middlewares (): void {
     this.app.use(express.json())
-    this.app.use(cors(options))
   }
 
   routes (): void {
